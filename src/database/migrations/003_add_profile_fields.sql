0d0e85ebecd524b0c27782eb8475add7afa70233
Piotr Szczodrowski <s32698@pjwstk.edu.pl>
2026-06-18 23:03:35 +0200

    fix ignored files

--- src/database/migrations/003_add_profile_fields.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `users`
-    ADD COLUMN `bio` VARCHAR(255) NULL,
-  ADD COLUMN `avatar_path` VARCHAR(255) DEFAULT '/uploads/avatars/default.png',
-  ADD COLUMN `birth_date` DATE NULL,
-  ADD COLUMN `project_evaluation` VARCHAR(50) NULL;
